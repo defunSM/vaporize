a6c346a6fd3c89c8dd81e82980d0a7dd836cf09a
defunsm <salmanhossain500@gmail.com>
2021-08-25 00:05:17 -0400

    abstract class Entity

--- enemy.py
+++ enemy.py
@@ -1,12 +1,30 @@
 import pygame
 import os
+from typing import List
+from abc import ABC, abstractmethod
 
 ENEMY_WIDTH, ENEMY_HEIGHT = 91, 91
 BLUE_UFO_HEALTH, BLUE_UFO_ARMOR = 100, 0
 
-class Enemy(pygame.sprite.Sprite):
+class Entity(ABC):
+    @abstractmethod
+    def animate(self):
+        pass
+    
+    @abstractmethod
+    def update(self):
+        pass
+
+class Enemy(pygame.sprite.Sprite, Entity):
     """Initializing player sprites with pygame.image.load"""
-    def __init__(self, pos_x, pos_y, assets):
+    def __init__(self, pos_x: int, pos_y: int, assets: List[str]):
+        """Most enemy npcs will inherit from this class.
+
+        Args:
+            pos_x ([int]): x position of the sprite
+            pos_y ([int]): y position of the sprite
+            assets ([List[str]]): path to the sprite image
+        """
         super().__init__()
         self.pos_x = pos_x
         self.pos_y = pos_y
@@ -51,8 +69,8 @@ class BlueEnemyUFO(Enemy):
         
     def animate(self):
         """Animates the Idle animation for the sprite"""
-        super(Enemy, self).animate()
+        super(BlueEnemyUFO, self).animate()
     
     def update(self, speed):
         """Updates the sprite for UFO, speed is an float between 0 and 1 to control speed of animation."""
-        super(Enemy, self).update()
+        super(BlueEnemyUFO, self).update(speed)
